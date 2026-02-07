12062e0b8c1b8e5ad2d525ad2711533c924492fd
GORE <149291163+GORE-MODS@users.noreply.github.com>
2026-02-07 17:48:48 -0600

    Update PluginInfo with new version and URL
    Updated version number and added VersionUrl constant.

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,10 +1,13 @@
-﻿namespace UnneededInfo
+namespace UnneededInfo
 {
     public class PluginInfo
     {
         public const string Name = "UnneededInfo";
         public const string Description = "A mod that info to the adds info to the coc board.";
         public const string GUID = "com.gore.unneededinfo";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
+
+        public const string VersionUrl =
+            "https://raw.githubusercontent.com/GORE-MODS/Unneeded-Info/main/newestv.txt";
     }
 }
