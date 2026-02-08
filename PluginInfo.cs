7d6243b512a581f395c407e451dc7bb232b3343e
GORE <149291163+GORE-MODS@users.noreply.github.com>
2026-02-08 14:40:09 -0600

    Update PluginInfo version to 4.0.0
    Updated version number from 1.0.1 to 4.0.0 in PluginInfo.

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,9 +5,10 @@ namespace UnneededInfo
         public const string Name = "UnneededInfo";
         public const string Description = "A mod that info to the adds info to the coc board.";
         public const string GUID = "com.gore.unneededinfo";
-        public const string Version = "1.0.1";
+        public const string Version = "4.0.0";
 
         public const string VersionUrl =
             "https://raw.githubusercontent.com/GORE-MODS/Unneeded-Info/main/newestv.txt";
     }
 }
+
